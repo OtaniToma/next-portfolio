84c17745404188bcea71a37caa2b8a611f78f97f
OtaniToma <otani.toma@gmail.com>
2023-05-01 10:10:47 +0900

    add top component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,14 @@
-import styles from "../styles/Sidebar.module.scss"
+import styles from "../styles/Sidebar.module.scss";
 
 export default function Sidebar() {
   return (
     <>
       <aside className={styles.sidebar}>
         <div className={styles.container}>
-          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/34960309" />
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/34960309"
+          />
           <style jsx global>{`
             .avatar {
               border-radius: 50%;
@@ -18,5 +21,5 @@ export default function Sidebar() {
         </div>
       </aside>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Layout from "../components/layout";
 import Sidebar from "../components/Sidebar";
 import Main from "../components/Main";
-import 'normalize.css/normalize.css';
+import Top from "../components/Top";
+import "normalize.css/normalize.css";
 
 export default function Home() {
   return (
     <>
       <Layout>
         <Sidebar />
-        <Main />
+        <Main>
+          <Top />
+        </Main>
       </Layout>
     </>
-  )
+  );
 }

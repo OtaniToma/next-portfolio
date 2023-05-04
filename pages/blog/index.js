785cd5b5a7ce929da2a1740c25005fe6421daf80
OtaniToma <otani.toma@gmail.com>
2023-05-04 16:37:12 +0900

    get blog articles from microcms

---
+++ pages/blog/index.js
@@ -0,0 +1,33 @@
+import Layout from "../../components/layout";
+import Sidebar from "../../components/Sidebar";
+import Main from "../../components/Main";
+import { getAllPosts } from "../../lib/api";
+
+export default function Blog({ posts }) {
+  console.log(posts);
+
+  return (
+    <Layout>
+      <Sidebar />
+      <Main>Blog</Main>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts();
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty("eyecatch")) {
+      post.eyecatch = eyecatchLocal;
+    }
+    // const { base64 } = await getPlaiceholder(post.eyecatch.url);
+    // post.eyecatch.blurDataURL = base64;
+  }
+
+  return {
+    props: {
+      posts: posts,
+    },
+  };
+}

d567202d257790809cc6839d004566eea3355c33
OtaniToma <otani.toma@gmail.com>
2023-05-05 10:11:42 +0900

    apply normalize css

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Layout from "../components/layout";
 import Sidebar from "../components/Sidebar";
 import Main from "../components/Main";
 import Top from "../components/Top";
-import "normalize.css/normalize.css";
 
 export default function Home() {
   return (
